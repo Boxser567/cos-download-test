cc8fb87c2a28d2628484ec6e09d104980de0a19f
shd <shd@gokuai.cn>
2017-08-18 18:08:10 +0800

    asdaaa

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -4,71 +4,117 @@
     <input type="text" v-model="fileurl" />
     <br/>
     <div class="tl">{{fileurl}}</div>
-    <button @click="doSomething">点我测试</button>
+    <button @click="doSomething">点我上传</button>
+    <br />
+  
+    <button @click="beginSomething">开始</button>
+    <button @click="pauseSomething">点我暂停</button>
   </div>
 </template>
 
 <script>
-import {remote} from 'electron'
+import { remote } from 'electron'
 import fs from 'fs'
 import path from 'path'
 import COS from 'cos-nodejs-sdk-v5'
 
-let cos = new COS({AppId :'1253834952',SecretId : 'AKIDa4NkxzaV0Ut7Yr4sa6ScbNwMdibHb4A4',SecretKey : 'qUwCGAsRq46wZ1HLCrKbhfS8e0A8tUu8'})
-  export default {
-    name: 'landing-page',
-    data(){
-      return{
-        fileurl:'http://111en-1253834952.cn-southwest.myqcloud.com/1.rar?sign=q-sign-algorithm%3Dsha1%26q-ak%3DAKIDa4NkxzaV0Ut7Yr4sa6ScbNwMdibHb4A4%26q-sign-time%3D1502971373%3B1506571373%26q-key-time%3D1502971373%3B1506571373%26q-header-list%3D%26q-url-param-list%3D%26q-signature%3D7e71231405aa282259ed8ca3c4dddddff51dd9b3'
-      }
+let cos = new COS({ AppId: '1253834952', SecretId: 'AKIDa4NkxzaV0Ut7Yr4sa6ScbNwMdibHb4A4', SecretKey: 'qUwCGAsRq46wZ1HLCrKbhfS8e0A8tUu8' })
+export default {
+  name: 'landing-page',
+  data() {
+    return {
+      fileurl: 'http://111en-1253834952.cn-southwest.myqcloud.com/1.rar?sign=q-sign-algorithm%3Dsha1%26q-ak%3DAKIDa4NkxzaV0Ut7Yr4sa6ScbNwMdibHb4A4%26q-sign-time%3D1503036804%3B1506636804%26q-key-time%3D1503036804%3B1506636804%26q-header-list%3D%26q-url-param-list%3D%26q-signature%3D215c5a3c270a423d22f083b758c3d4d1fcf54682'
+    }
+  },
+  methods: {
+    doSomething() {
+      // console.log('__dirname',__dirname)
+      // console.log('cos',cos)
+      remote.dialog.showOpenDialog({
+        buttonLabel: '选择',
+        filters: [{ name: 'All Files', extensions: ['*'] }],
+        properties: ['openDirectory']
+      }, savepath => {
+        console.log(savepath[0])
+        let list = [{ path: 'http://111en-1253834952.cn-southwest.myqcloud.com/1.rar?sign=q-sign-algorithm%3Dsha1%26q-ak%3DAKIDa4NkxzaV0Ut7Yr4sa6ScbNwMdibHb4A4%26q-sign-time%3D1503036804%3B1506636804%26q-key-time%3D1503036804%3B1506636804%26q-header-list%3D%26q-url-param-list%3D%26q-signature%3D215c5a3c270a423d22f083b758c3d4d1fcf54682', key: '1.rar' },
+        { path: 'http://111en-1253834952.cn-southwest.myqcloud.com/12.rar?sign=q-sign-algorithm%3Dsha1%26q-ak%3DAKIDa4NkxzaV0Ut7Yr4sa6ScbNwMdibHb4A4%26q-sign-time%3D1503040864%3B1506640864%26q-key-time%3D1503040864%3B1506640864%26q-header-list%3D%26q-url-param-list%3D%26q-signature%3Dbad82859f02316e4695eb776a4e57b5dc363fd27', key: '12.rar' },
+        { path: 'http://111en-1253834952.cn-southwest.myqcloud.com/2.rar?sign=q-sign-algorithm%3Dsha1%26q-ak%3DAKIDa4NkxzaV0Ut7Yr4sa6ScbNwMdibHb4A4%26q-sign-time%3D1503040906%3B1506640906%26q-key-time%3D1503040906%3B1506640906%26q-header-list%3D%26q-url-param-list%3D%26q-signature%3D2108ec72e5b15a0e77620686071ab3020ce669f5', key: '2.rar' }
+        ]
+        list.splice(1, 1)
+        for (let obj in list) {
+          
+          let rs = fs.createReadStream(`${savepath[0]}/${list[obj].key}.tmp`);
+          let ws = fs.createWriteStream(`${savepath[0]}/${list[obj].key}.tmp`)
+          rs.pipe(ws);
+          rs.on('data', function (data) {
+            console.log('数据可读', arguments)
+          });
+          rs.on('end', function () {
+            console.log('文件读取完成', arguments);
+          });
+          cos.getObject({
+            Bucket: '111en',
+            Region: 'cn-southwest',
+            Key: list[obj].key,
+            Path: list[obj].path,
+            Output: ws,//savepath[0]+'/xxyy.tmp'
+            onProgress: function (progressData) {
+              console.log(1, progressData);
+            }
+          }, function (err, data) {
+            console.log(arguments)
+          });
+        }
+      })
     },
-    methods: {
-      doSomething(){
-        // console.log('__dirname',__dirname)
-        // console.log('cos',cos)
-    remote.dialog.showOpenDialog({
-      buttonLabel: '选择',
-      filters: [{name: 'All Files', extensions: ['*']}],
-      properties: ['openDirectory']
-    },savepath=>{
-        cos.getObject({
+    pauseSomething() {
+      let params = {
         Bucket: '111en',
         Region: 'cn-southwest',
-        Key: "1.rar",
-        Path:savepath[0],
-        Prefix:this.fileurl,
-        Output: fs.createWriteStream('mytest.tmp'),
-        onProgress: function (progressData) {
-            console.log(1,progressData);
-        }
-    }, function (err, data) {
-      console.log(arguments)
-    });
-    })
-    
-
-
+        Key: list[obj].key,
+        Path: list[obj].path,
+        dasd: 1
       }
+      params.Body.emit('error', new Error('cancel'))
+      let rs = fs.createReadStream('/Users/gokuai/Downloads/2.rar.tmp');
+      let ws = fs.createWriteStream('/Users/gokuai/Downloads/2.rar.tmp')
+
+
+      // rs.pipe(ws);
+      // console.log(rs)
+      // rs.on('data', function (data) {
+      //   console.log('数据可读',arguments)
+      // });
+      // rs.on('end', function (data) {
+      //   console.log('文件读取完成',data);
+      // });
+    },
+    beginSomething() {
+
     }
   }
+}
 </script>
 
 
 
 <style scoped>
-#wrapper{
-margin-left: 20%
+#wrapper {
+  margin-left: 20%
 }
-input{
+
+input {
   width: 260px;
   padding: 8px 15px;
   margin-bottom: 14px;
 }
-.tl{
+
+.tl {
   padding: 10px 0;
   color: #999;
 }
-button{
+
+button {
   padding: 8px 100px;
 }
 </style>
